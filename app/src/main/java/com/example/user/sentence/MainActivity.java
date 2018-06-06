0e15df758b843318744702d5f1f828b4ed625a84
kirtox <asxz80490@gmail.com>
2018-06-06 19:36:40 +0800

    6/6_0736

--- app/src/main/java/com/example/user/sentence/MainActivity.java
+++ app/src/main/java/com/example/user/sentence/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.user.sentence;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.database.Cursor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.lang.String;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mSpn; // mSpn is object of Spinner
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSpn = (Spinner) findViewById(R.id.spn_mode);
+        //mSpn.setOnItemSelectedListener(spnOnItemSelected);
+
+
     }
+
+
+
 }
+
